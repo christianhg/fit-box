9a2da6efc0b1eb7cef140c683fb05a48013f9bbc
Christian Hamburger Grøngaard <christianhg@gmail.com>
2019-08-31 19:48:48 +0200

    Replace own box generator with one from npm

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,32 +1,20 @@
 import { fitBox } from '../src';
 import { aspectRatio } from '../src/util';
-import { createBoxGenerator } from './box-generator';
+import { createBoxGenerator } from 'box-generator';
 import { contains } from './contains';
 import { randomNumber } from './random-number';
 
 describe(fitBox.name, () => {
   it('works', () => {
     const boxes = createBoxGenerator({
-      length: randomNumber({ from: 1, to: 1000 }),
+      initial: {
+        width: randomNumber({ from: 1, to: 1000 }),
+        height: randomNumber({ from: 1, to: 1000 }),
+      },
     });
 
     for (let box of boxes) {
-      const boundaries = [
-        ...createBoxGenerator({
-          length: randomNumber({ from: box.width + 1, to: box.width * 5 }),
-        }),
-        ...createBoxGenerator({
-          length: randomNumber({ from: box.height + 1, to: box.height * 5 }),
-        }),
-        ...createBoxGenerator({
-          length: randomNumber({ from: 1, to: box.width - 1 }),
-        }),
-        ...createBoxGenerator({
-          length: randomNumber({ from: 1, to: box.height - 1 }),
-        }),
-        ...createBoxGenerator({ length: box.width }),
-        ...createBoxGenerator({ length: box.height }),
-      ];
+      const boundaries = createBoxGenerator({ initial: box });
 
       for (let boundary of boundaries) {
         const fittedBox = fitBox({ boundary, box });
